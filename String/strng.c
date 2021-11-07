7320d097a6b2d9ec266a1c5dd6c551ce2859c53a
Mehedi Hasan <93500871+mH-13@users.noreply.github.com>
2021-11-08 04:40:51 +0600

    Update strng.c

--- String/strng.c
+++ String/strng.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
+int main()
+{
     char s[1002], ch;
     int i, len, is_word_started;
-    get(s);
+    gets(s);
+    len = strlen(s);
     is_word_started = 0;
-    for(i=0; i < len; i++){
-        if(s[i] >= 'a' && s[i] <= 'z'){
-            if(is_word_started==0){
-                is_word_started =1;
-                ch ='A' + s[i] - 'a';
+    for (i = 0; i < len; i++)
+    {
+        if (s[i] >= 'a' && s[i] <= 'z')
+        {
+            if (is_word_started == 0)
+            {
+                is_word_started = 1;
+                ch = 'A' + s[i] - 'a';
                 printf("%c", ch);
             }
-            else{
+            else
+            {
                 printf("%c", s[i]);
             }
         }
-        else if((s[i]>='A' && s[i] <='Z') || (s[i] >='0' && s[i] <='9')){
-            if(is_word_started==0){
-                is_word_started =1;
+        else if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '0' && s[i] <= '9'))
+        {
+            if (is_word_started == 0)
+            {
+                is_word_started = 1;
             }
-            prinatf("%c", s[i]);
+            printf("%c", s[i]);
         }
-        else{
-            if(is_word_started==1){
-                is_word_started=0;
-                printf("\n";)
+        else
+        {
+            if (is_word_started == 1)
+            {
+                is_word_started = 0;
+                printf("\n");
             }
         }
     }
