2b20c29c55da786c4b7c44cc4505c95a1ec95162
Mehedi Hasan <93500871+mH-13@users.noreply.github.com>
2021-11-12 23:36:56 +0600

    Create Marks.c

---
+++ Array/2D Array/Marks.c
@@ -0,0 +1,17 @@
+//data type array name[number of row][number of columlns];
+//int marks[4][10]
+//1st row - marks[0], 2nd row - marks[1]
+//marks[0][0] - 1st row 1st column
+//marks[2][1] - 3rd row 2nd column
+//int marks[4][10] = {{0...9},{0....9},{0....9},{0....9}}
+
+#include <stdio.h>
+int main(){
+    int marks[4][10] = {{},{},{},{}};
+    int col;
+    for(col=0; col<10;col++){
+        marks[3][col] = marks[0][col]/ 4.0 + marks[1][col] /4.0 + marks[2][col]/ 2.0;
+        printf("Roll No: %d Total Marks: %d/n", col +1, marks[3][col]);
+    }
+    return 0;
+}
